3a653c26c612f1691de0a21b9064a4add0a2f77a
Ningxin Liao <ningxin.liao@mutualmobile.com>
2018-03-13 17:29:31 -0500

    Added the desiredCapabilities that work for IE.

--- project.py
+++ project.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
 from uiautomation_pkg_common_webdriver import *
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def main():
-    driver = webdriver.Edge()
+    # desired_caps = DesiredCapabilities.INTERNETEXPLORER
+    # desired_caps['se:ieOptions'] = {}
+    # desired_caps['se:ieOptions']['ie.ensureCleanSession'] = True
+    # desired_caps['se:ieOptions']['ignoreZoomSetting'] = True
+    # desired_caps['se:ieOptions']['enablePersistentHover'] = False
+    # desired_caps['se:ieOptions']['nativeEvents'] = True
+    # desired_caps['se:ieOptions']['requireWindowFocus'] = True
+    # driver = webdriver.Ie(capabilities=desired_caps)
+    driver = webdriver.Chrome()
     global app
     global UIType
     app = Device(driver)
@@ -12,3 +21,4 @@ def main():
     app.driver.get('https://southwire-configurator-test.firebaseapp.com/')
 
 main()
+
--- projectBase.py
+++ projectBase.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
 from unittest import TestCase
 from uiautomation_pkg_common_webdriver import *
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class ProjectBase(TestCase):
     def setUp(self):
-        self.driver = webdriver.Edge()
+        # #The following code is needed to run IE in order to clear cache
+        # desired_caps = DesiredCapabilities.INTERNETEXPLORER
+        # desired_caps['se:ieOptions'] = {}
+        # desired_caps['se:ieOptions']['ie.ensureCleanSession'] = True
+        # desired_caps['se:ieOptions']['ignoreZoomSetting'] = True
+        # desired_caps['se:ieOptions']['enablePersistentHover'] = True
+        # desired_caps['se:ieOptions']['nativeEvents'] = False
+        # desired_caps['se:ieOptions']['requireWindowFocus'] = True
+        # self.driver = webdriver.Ie(capabilities=desired_caps)
+        self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(5)
         self.app = Device(self.driver)
         self.UIType = Type(self.driver)
@@ -16,5 +26,9 @@ class ProjectBase(TestCase):
         self.isSafari = self.app.isSafari()
         self.driver.maximize_window()
 
+
+
+
     def tearDown(self):
+        self.driver.close()
         self.driver.quit()
