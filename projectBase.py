e5d8eaca4b8008622a2f49a144f63d29d5269bf6
Khai Le <khai.le@mutualmobile.com>
2018-05-03 17:05:30 -0500

    updated tearDown in projectBase to handle various browsers

--- projectBase.py
+++ projectBase.py
@@ -3,8 +3,6 @@ from unittest import TestCase
 from uiautomation_pkg_common_webdriver import *
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
-import sys
-
 
 class ProjectBase(TestCase):
     def setUp(self):
@@ -14,7 +12,7 @@ class ProjectBase(TestCase):
         runId = 'placeholderRunId'
         self.caseId = ''
         self.client = APIClient(runId=runId, version=version, user=user, key=key)
-        # #The following code is needed to run IE in order to clear cache
+        # """The following code is needed to run IE in order to clear cache"""
         # desired_caps = DesiredCapabilities.INTERNETEXPLORER
         # desired_caps['se:ieOptions'] = {}
         # desired_caps['se:ieOptions']['ie.ensureCleanSession'] = True
@@ -23,7 +21,7 @@ class ProjectBase(TestCase):
         # desired_caps['se:ieOptions']['nativeEvents'] = True
         # desired_caps['se:ieOptions']['requireWindowFocus'] = True
         # self.driver = webdriver.Ie(capabilities=desired_caps)
-
+        # """End of IE block"""
 
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(15)
@@ -34,6 +32,8 @@ class ProjectBase(TestCase):
         self.isMobile = self.app.isMobile()
         self.isChromium = False
         self.isSafari = self.app.isSafari()
+        self.isInternetExplorer = self.app.isInternetExplorer()
+        self.isFirefox = self.app.isFirefox()
         self.driver.maximize_window()
 
         self.screenshotPath = '../../screenshots/'
@@ -53,9 +53,17 @@ class ProjectBase(TestCase):
         except:
             pass
         finally:
-            self.driver.close()
-            if self.app.isInternetExplorer():
+            if self.isInternetExplorer():
                 """Make sure that all IE instances are closed since each run opens 2 IE instances"""
                 os.system('TASKKILL /F /IM iexplore.exe /T')
-            if not self.app.isFirefox():
+
+            elif self.isSafari and self.assertion.didThrowError():
+                """Make sure that Safari Automated Session is killed before starting a new one"""
+                try:
+                    os.system('kill $(ps aux | grep "Safari --automation" | awk "{print $2}")')
+                except:
+                    pass
+            else:
+                self.driver.close()
+            if not (self.isFirefox() or self.isSafari()):
                 self.driver.quit()
